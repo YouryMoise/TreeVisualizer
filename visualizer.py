1f8542dfaf71c50aadcbd174c96c816e1d625945
Youry Moise <yourym@mit.edu>
2023-10-13 22:28:54 -0400

    Added preorder and postorder traversals

--- visualizer.py
+++ visualizer.py
@@ -2,6 +2,9 @@
 from tkinter import *
 import tkinter as tk
 import time
+from tkinter.ttk import *
+from functools import partial
+
 
 
 exTree = [0,
@@ -204,6 +207,82 @@ def get_next_step(tree):
                 return new_tree
     return tree
 
+def get_next_step_preorder(tree):
+    #expecting properly formatted tree
+
+    #pre order is node, node.left, node.right
+    #if current node is not colored, color it
+    if tree[3] == 0:
+        return [tree[0], tree[1], tree[2], 1]
+    
+    #if current is colored, check left node
+    if tree[1] and tree[1][3] == 0:
+        subTree = get_next_step_preorder(tree[1])
+        return [tree[0], subTree, tree[2], tree[3]]
+    #if left node colored, still need to make sure left node kids are good
+    if tree[1] and tree[1][3] == 1:
+        subTree = get_next_step_preorder(tree[1])
+        #return if it made a difference
+        if subTree != tree[1]:
+            print('h')
+            return [tree[0], subTree, tree[2], tree[3]]
+    
+    #same logic for right child
+    if tree[2] and tree[2][3] == 0:
+        subTree = get_next_step_preorder(tree[2])
+        return [tree[0], tree[1], subTree, tree[3]]
+    #if left node colored, still need to make sure left node kids are good
+    if tree[2] and tree[2][3] == 1:
+        subTree = get_next_step_preorder(tree[2])
+        #return if it made a difference
+        if subTree != tree[2]:
+            return [tree[0], tree[1], subTree, tree[3]]
+
+    return tree
+
+def get_next_step_postorder(tree):
+    #expecting properly formatted tree
+
+    #pre order is node.left, node.right, node
+    #if current node is not colored, color it
+    
+    
+    if tree[1] and tree[1][3] == 0:
+        subTree = get_next_step_postorder(tree[1])
+        return [tree[0], subTree, tree[2], tree[3]]
+    #if left node colored, still need to make sure left node kids are good
+    if tree[1] and tree[1][3] == 1:
+        subTree = get_next_step_postorder(tree[1])
+        #return if it made a difference
+        if subTree != tree[1]:
+            return [tree[0], subTree, tree[2], tree[3]]
+    
+    #same logic for right child
+    if tree[2] and tree[2][3] == 0:
+        subTree = get_next_step_postorder(tree[2])
+        return [tree[0], tree[1], subTree, tree[3]]
+    #if left node colored, still need to make sure left node kids are good
+    if tree[2] and tree[2][3] == 1:
+        subTree = get_next_step_postorder(tree[2])
+        #return if it made a difference
+        if subTree != tree[2]:
+            return [tree[0], tree[1], subTree, tree[3]]
+    
+    #if current is colored, check left node
+    if tree[3] == 0:
+        return [tree[0], tree[1], tree[2], 1]
+
+    return tree
+
+smallTree = [8,[2],[9]]
+fullSmallTree = [8, [2, [], [], 0], [9, [], [], 0], 0]
+t = get_next_step_preorder(fullSmallTree)
+
+for i in range(3):
+    print(t)
+    t = get_next_step_preorder(t)
+
+
 
 
 def count_nodes(tree):
@@ -219,11 +298,17 @@ def count_nodes(tree):
 
 #print(count_nodes(insert_empty_lists(insert_zero(exTree))))
 
-def get_all_steps(tree):
+def get_all_steps(tree, traversalType):
     """
     A generator that yields a list representation for what the tree
     should look like at every step of the in_order traversal
     """
+    if traversalType == 'inorder':
+        func = get_next_step
+    elif traversalType == 'preorder':
+        func = get_next_step_preorder
+    elif traversalType == 'postorder':
+        func = get_next_step_postorder
     #also assuming well-formatted tree
     current_tree = tree
     #for i in range(steps+1):
@@ -234,7 +319,7 @@ def get_all_steps(tree):
     while current_tree != previous_tree:
         previous_tree = current_tree
         if current_tree is not None:
-            current_tree = get_next_step(current_tree)
+            current_tree = func(current_tree)
         yield current_tree
     
     #return all_trees
@@ -247,16 +332,16 @@ def recursive_drawer(list_of_reps):
     """
     global c
     c.delete('all')
-    if list_of_reps[0] == None:
+    if not list_of_reps:
         return None
     draw_node(c, list_of_reps[0])
-    win.after(200, recursive_drawer, list_of_reps[1:])
+    win.after(500, recursive_drawer, list_of_reps[1:])
     
 
-def in_order_2_wrapper():
+def traversal_wrapper(traversalType):
     global name_entry
     fixedTree = insert_empty_lists(insert_zero(parse_string_to_list(name_entry.get())))
-    queue = [element for element in get_all_steps(fixedTree)]
+    queue = [element for element in get_all_steps(fixedTree,traversalType)]
     recursive_drawer(queue)
 
 def onClick():
@@ -279,11 +364,27 @@ name_entry = tk.Entry(win, text = exTreeString,font=('calibre',10,'normal'))
 
 name_entry.place(x=10, y=10)
 
-myButton = Button(win, command = onClick)
-myButton.place(x=10,y=20)
+myButton = Button(win, command = onClick, text = "Visualize")
+myButton.place(x=200,y=10)
 
-traversalButton = Button(win, command = in_order_2_wrapper)
-traversalButton.place(x=10,y=30)
+style = Style()
+ 
+# This will be adding style, and 
+# naming that style variable as 
+# W.Tbutton (TButton is used for ttk.Button).
+style.configure('W.TButton', font =
+               ('calibri', 10, 'bold', 'underline'),
+                foreground = 'red')
+ 
+
+traversalButton = Button(win, style = 'W.TButton',text = "Inorder traversal",command = partial(traversal_wrapper,'inorder'))
+traversalButton.place(x=200,y=60)
+
+preTraversalButton = Button(win, style = 'W.TButton',text = "Preorder traversal",command = partial(traversal_wrapper,'preorder'))
+preTraversalButton.place(x=200,y=110)
+
+preTraversalButton = Button(win, style = 'W.TButton',text = "Postorder traversal",command = partial(traversal_wrapper,'postorder'))
+preTraversalButton.place(x=200,y=160)
 #draw_node(c,name_entry.get())
 #name_var=tk.StringVar("HELLO")
 
